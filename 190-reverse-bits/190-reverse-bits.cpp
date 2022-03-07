2ad2f65f561880c6bef51f82c816fc3f7a5d78b2
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-03-06 17:14:15 -0800

    Time: 8 ms (10.16%), Space: 6.2 MB (10.24%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.cpp
+++ 190-reverse-bits/190-reverse-bits.cpp
@@ -1,14 +1,35 @@
 class Solution {
 public:
-    
+    uint32_t reverseByte(uint32_t byte, map<uint32_t, uint32_t>& cache)
+    {
+        if(cache.find(byte) != cache.end())
+        {
+            cout << "byte : " << byte << " " << cache[byte]<<endl;
+            return cache[byte];
+        }
+        uint32_t temp = byte;    
+        uint32_t value = 0;
+        int pos = 7;
+        while(temp)
+        {
+            value += (temp & 1) << pos;
+            temp = temp >> 1;
+            pos--;
+        }
+        cout << "Ibyte : " << byte << " " << cache[byte]<<endl;
+        cache[byte] = value;    
+        return value;
+    }
     uint32_t reverseBits(uint32_t n) {
         uint32_t result = 0; 
-        int pos = 31;
+        int pos = 24;
+        map<uint32_t, uint32_t> cache;
         while(n)
         {
-            result |= (n & 1) << pos;
-            n = n >> 1;
-            pos--;
+            uint32_t byte = n & 0xFF;
+            result |= reverseByte(byte, cache) << pos;
+            n = n >> 8;
+            pos -= 8;
         }
         return result;
     }
