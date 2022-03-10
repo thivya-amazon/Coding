7cfd74b5d3f77bac40616070db269040410f6b25
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-03-09 20:35:18 -0800

    Time: 8 ms (54.95%), Space: 10.3 MB (75.48%) - LeetHub

---
+++ 86-partition-list/86-partition-list.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode* sentinel = new ListNode(INT_MIN, NULL);
+        ListNode* tail = sentinel;
+        ListNode* greater = NULL;
+        ListNode* greaterHead = NULL;
+        ListNode* curr = head;
+        while(curr)
+        {
+            ListNode* succ = curr->next;
+            curr->next = NULL;
+            if(curr->val < x)
+            {
+                tail->next = curr;
+                tail = curr;
+            }
+            else
+            {
+                if(!greater)
+                {
+                    greaterHead = curr;
+                    greater = curr;
+                }
+                else
+                {
+                    greater->next = curr;
+                    greater = curr;
+                }
+            }
+            curr = succ;
+        }
+        if(tail)
+            tail->next = greaterHead;
+        return sentinel->next;
+        
+    }
+};
