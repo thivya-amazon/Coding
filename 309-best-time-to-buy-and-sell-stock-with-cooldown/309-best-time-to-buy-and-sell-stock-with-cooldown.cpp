8d73187ab8f10285049d6b4f38865b76bb778562
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-03-17 16:15:20 -0700

    Time: 3 ms (86.15%), Space: 11.2 MB (83.82%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -3,16 +3,15 @@ public:
     vector<int> global;
     int maxProfit(vector<int>& prices) {
     int n = prices.size();
-    vector<int> local(n);
+    int local = 0;
     global.resize(n);
         
-        local[0] = 0;
         global[0] = 0;
         
         for(int i = 1; i < n; i++)
         {
-            local[i] = prices[i] - prices[i-1] + max(getGlobal(i-3), local[i-1]);
-            global[i] = max(global[i-1], local[i]);
+            local = prices[i] - prices[i-1] + max(getGlobal(i-3), local);
+            global[i] = max(global[i-1], local);
         }
         return global[n-1];
         
