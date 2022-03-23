0dc74bc751a64b4c373d6afdc72c35158964b00c
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-03-23 16:09:34 -0700

    Time: 110 ms (50.49%), Space: 35.6 MB (92.75%) - LeetHub

---
+++ 716-max-stack/716-max-stack.cpp
@@ -0,0 +1,90 @@
+class MaxStack {
+public:
+    stack<pair<int,int>> maxStack;
+    stack<int> aux;
+    MaxStack() {
+        
+    }
+    
+    void push(int x) {
+       if(!maxStack.empty())
+       {
+           int maxVal = max(maxStack.top().second, x);
+           maxStack.push({x, maxVal});
+       }
+        else
+            maxStack.push({x, x});
+    }
+    
+    int pop() {
+        int elem;
+        if(!maxStack.empty())
+        {
+            elem = maxStack.top().first;
+            maxStack.pop();
+        }
+        return elem;    
+    }
+    
+    int top() {
+        int elem;
+        if(!maxStack.empty())
+        {
+            elem = maxStack.top().first;
+        }
+        return elem;
+    }
+    
+    int peekMax() {
+        int elem;
+        if(!maxStack.empty())
+        {
+            elem = maxStack.top().second;
+        }
+        return elem;
+        
+    }
+    
+    int popMax() {
+        int maxVal = peekMax();
+        //Move all values to an aux stack until we get to the max value
+        while(!maxStack.empty() && top() != maxVal)
+        {
+            aux.push(top());
+            maxStack.pop();
+        }
+        //Remove the max element
+        maxStack.pop();
+        
+        //Push back values from aux stack to maxStack with an updated maxValue
+        while(!aux.empty())
+        {
+            int elemToAdd = aux.top();
+            if(!maxStack.empty())
+            {
+                int currMax = peekMax();
+                elemToAdd = aux.top();
+                if( currMax >= elemToAdd)
+                    maxStack.push({elemToAdd, currMax});
+                else
+                    maxStack.push({elemToAdd, elemToAdd});
+            }
+            else
+            {
+                 maxStack.push({elemToAdd, elemToAdd});
+            }
+            aux.pop();
+        }
+        return maxVal;
+    }
+};
+
+/**
+ * Your MaxStack object will be instantiated and called as such:
+ * MaxStack* obj = new MaxStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->peekMax();
+ * int param_5 = obj->popMax();
+ */
