714da321dae2de2b428eec3e1870bf3ce5e73620
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-03-23 22:20:53 -0700

    Time: 190 ms (74.15%), Space: 84.9 MB (95.99%) - LeetHub

---
+++ 739-daily-temperatures/739-daily-temperatures.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int> answer(temperatures.size(), 0);
+        //Mono stack holds the indices
+        stack<int> mono;
+        for(int i = temperatures.size()-1; i >= 0; i--)
+        {
+            while(!mono.empty() && temperatures[i] >= temperatures[mono.top()])
+                mono.pop();
+            if(!mono.empty())
+                answer[i] = mono.top() - i;
+            mono.push(i);
+        }
+        return answer;
+    }
+};
