fd74e518a17bdb5c7f29c809caf1bfd8a908f2ae
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-03-27 12:04:28 -0700

    Time: 2 ms (44.91%), Space: 6.5 MB (49.71%) - LeetHub

---
+++ 388-longest-absolute-file-path/388-longest-absolute-file-path.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    int lengthLongestPath(string input) {
+        string item;
+        stringstream ss(input);
+        size_t found;
+        int maxLength = 0;
+        unordered_map<int, int> pathLength;
+        while(!ss.eof())
+        {
+            getline(ss, item, '\n');
+            int numTabs = 0;
+            while(item[numTabs] == '\t')
+                numTabs++;
+            //root level
+            if(numTabs == 0)
+                pathLength[numTabs] = item.size();
+            else
+            {
+                pathLength[numTabs] = pathLength[numTabs-1] + 1 + item.substr(numTabs).size();
+            }
+            if(item.find('.') != string::npos)
+                maxLength = max(maxLength, pathLength[numTabs]);
+        }
+       return maxLength; 
+        
+    }
+};
