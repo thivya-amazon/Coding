f46f53f7c08e1ba1bde798bd93add2a07967529d
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-03-26 18:50:52 -0700

    Time: 22 ms (81.42%), Space: 11.7 MB (45.06%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -2,41 +2,32 @@ class Solution {
 public:
     string minRemoveToMakeValid(string s) {
         string result = "";
-        stack<pair<char, int>> stk;
+        stack<int> stk;
         for(int i = 0; i < s.size(); i++)
         {
             if(s[i] == '(')
             {
-                result.push_back(s[i]);
-                stk.push({s[i], result.size()-1});
+                stk.push(i);
             }
             else if(s[i] == ')')
             {
-                if(!stk.empty())
-                {
-                    result.push_back(s[i]);
+                if(stk.empty())
+                   s[i] = '$';
+                else
                     stk.pop();
-                }                   
             }
-            else
-                result.push_back(s[i]);
         }
         while(!stk.empty())
         {
-            int length = result.size();
-            int index = stk.top().second;
-            int nextIdx = index+1;
-
-            if(nextIdx < length)
-            {
-                string sub = result.substr(nextIdx);
-                result.resize(index+length-nextIdx);
-                result.replace(index, length-nextIdx, sub);
-            }
-            else
-                result.pop_back();
+            s[stk.top()] = '$';
             stk.pop();
         }
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(s[i] != '$')
+                result.push_back(s[i]);
+        }
+            
        return result; 
     }
 };
