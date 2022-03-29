e0642a96f6b2628ae258ec154056635444e2d2eb
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-03-28 23:42:48 -0700

    Time: 8 ms (51.88%), Space: 16.8 MB (7.36%) - LeetHub

---
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isMirror(vector<int>& input)
+    {
+        int size = input.size();
+        for (int i = 0; i < size / 2; i++) {
+            if (input[i] != input[size - 1 - i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    bool isSymmetric(TreeNode* root) {
+        if(root == NULL)
+            return true;
+        queue<TreeNode*> q;
+        q.push(root);
+        int count = 0;
+       
+        while(!q.empty())
+        {
+            count = q.size();
+            vector<int> slate;
+            for(int i = 0; i < count; i++)
+            {
+                TreeNode* levelNode = q.front();
+                q.pop();
+
+                if(levelNode->left)
+                {
+                    slate.push_back(levelNode->left->val);
+                    q.push(levelNode->left);
+                }                    
+                else
+                    slate.push_back(INT_MIN);
+
+                if(levelNode->right)
+                {
+                    slate.push_back(levelNode->right->val);
+                    q.push(levelNode->right);
+                }                    
+                else
+                    slate.push_back(INT_MIN);
+
+            }
+            if(!isMirror(slate))
+                return false;
+        }
+        return true;
+        
+    }
+};
