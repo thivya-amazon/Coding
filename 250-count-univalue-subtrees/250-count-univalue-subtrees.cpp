b4c27d26187179ff2a1b0a7f775827b41c13a731
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-04-01 13:30:54 -0700

    Time: 0 ms (100.00%), Space: 16.5 MB (19.98%) - LeetHub

---
+++ 250-count-univalue-subtrees/250-count-univalue-subtrees.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int count = 0;
+    bool dfs(TreeNode* root)
+    {
+         bool isUnival = true;
+        //Leaf nodes
+        // if(root->left == NULL && root->right == NULL)
+        // {
+        //     count++;
+        //     return true;
+        // }
+        
+        //Internal nodes
+        if(root->left)
+        {
+            bool left = dfs(root->left);
+            if(!left || root->val != root->left->val)
+                isUnival = false;
+            
+        }
+        if(root->right)
+        {
+            bool right = dfs(root->right);
+            if(!right || root->val != root->right->val)
+                isUnival = false;            
+        }
+        if(isUnival)
+            count++;
+        return isUnival;
+    }
+    int countUnivalSubtrees(TreeNode* root) {
+        if(root == NULL)
+            return 0;
+        dfs(root);
+        return count;
+    }
+};
