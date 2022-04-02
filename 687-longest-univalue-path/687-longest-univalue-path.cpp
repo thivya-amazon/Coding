b254c44122d10d9b4af8c0eb99553c123f9dcbcb
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-04-02 16:16:28 -0700

    Time: 172 ms (62.55%), Space: 71.9 MB (19.79%) - LeetHub

--- 687-longest-univalue-path/687-longest-univalue-path.cpp
+++ 687-longest-univalue-path/687-longest-univalue-path.cpp
@@ -9,6 +9,10 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+/*
+Global problem - longest univalue path in the tree
+Local problem - longest univalue path through me in my subtree
+*/
 class Solution {
 public:
     int maxPathLen = 0;
@@ -17,27 +21,27 @@ public:
                 
         int left = 0;
         int right = 0;
-        int leftArrows = 0;
-        int rightArrows  = 0;
-        int arrowPath = 0;
+        int leftUnivalPath = 0;
+        int rightUnivalPath  = 0;
+        int myUnivalPath = 0;
 
         if(root->left)
         {
             left = dfs(root->left);
             if(root->val == root->left->val)
-                leftArrows = left + 1;
+                leftUnivalPath = left + 1;
         }
             
         if(root->right)
         {
             right = dfs(root->right);
             if(root->val == root->right->val)
-                rightArrows =right + 1;
+                rightUnivalPath =right + 1;
         }
-        arrowPath = max(leftArrows, rightArrows);
-       maxPathLen = max(maxPathLen, leftArrows+rightArrows); 
+       myUnivalPath = max(leftUnivalPath, rightUnivalPath);
+       maxPathLen = max(maxPathLen, leftUnivalPath+rightUnivalPath); 
         
-        return(arrowPath);
+        return(myUnivalPath);
             
     }
     int longestUnivaluePath(TreeNode* root) {
