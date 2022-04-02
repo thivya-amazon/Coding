f9e42809b33c04d367464c7f59470d03702e8ba8
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-04-02 15:34:55 -0700

    Time: 164 ms (70.35%), Space: 71.9 MB (47.17%) - LeetHub

--- 687-longest-univalue-path/687-longest-univalue-path.cpp
+++ 687-longest-univalue-path/687-longest-univalue-path.cpp
@@ -14,20 +14,30 @@ public:
     int maxPathLen = 0;
     int dfs(TreeNode* root)
     {
-        if(root == NULL)
-            return 0;
-        int left = dfs(root->left);
-        int right = dfs(root->right);
-        
+                
+        int left = 0;
+        int right = 0;
         int leftArrows = 0;
-        int rightArrows = 0;
-        if(root->left && root->val == root->left->val)
-            leftArrows = left+1;
-        if(root->right && root->val == root->right->val)
-            rightArrows = right+1;
+        int rightArrows  = 0;
+        int arrowPath = 0;
+
+        if(root->left)
+        {
+            left = dfs(root->left);
+            if(root->val == root->left->val)
+                leftArrows = left + 1;
+        }
+            
+        if(root->right)
+        {
+            right = dfs(root->right);
+            if(root->val == root->right->val)
+                rightArrows =right + 1;
+        }
+        arrowPath = max(leftArrows, rightArrows);
        maxPathLen = max(maxPathLen, leftArrows+rightArrows); 
         
-        return(max(rightArrows, leftArrows));
+        return(arrowPath);
             
     }
     int longestUnivaluePath(TreeNode* root) {
