b99a65f2ea393ec1111692aae40d629cfbd041bd
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-04-02 17:43:47 -0700

    Time: 12 ms (82.31%), Space: 21.1 MB (55.23%) - LeetHub

---
+++ 333-largest-bst-subtree/333-largest-bst-subtree.cpp
@@ -0,0 +1,80 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unsigned int maxCount = 1;
+    class BST{
+        public:
+        int largest;
+        int smallest;
+        unsigned int count;
+        bool isBST;  
+        BST()
+        {
+            
+        }
+        BST(int l, int s, int c, bool b)
+        {
+            largest = l;
+            smallest = s;
+            count = c;
+            isBST = b;
+        }
+    };
+    BST dfs(TreeNode* node)
+    {
+        BST myValue;
+        BST leftValue;
+        BST rightValue;
+
+        if(node->left == NULL && node->right == NULL)
+        {
+            //return BST(INT_MAX, INT_MIN, 0, true);
+        }
+           
+        myValue.largest = node->val;
+        myValue.smallest = node->val;
+        myValue.count = 1;
+        myValue.isBST = true;       
+        
+        if(node->left)
+        {
+            leftValue = dfs(node->left);
+            myValue.count += leftValue.count;
+            myValue.largest = max(myValue.largest, leftValue.largest);
+            myValue.smallest = min(myValue.smallest, leftValue.smallest);
+            if(!leftValue.isBST || node->val <= leftValue.largest)
+                myValue.isBST = false;
+        }
+        if(node->right)
+        {
+            rightValue = dfs(node->right);
+            myValue.count += rightValue.count;
+            myValue.largest = max(myValue.largest, rightValue.largest);
+            myValue.smallest = min(myValue.smallest, rightValue.smallest);
+            if(!rightValue.isBST || node->val >= rightValue.smallest)
+                myValue.isBST = false;
+        }
+        if(myValue.isBST)
+        {
+            maxCount = max(maxCount, myValue.count);
+        }
+
+        return myValue;
+    }
+    int largestBSTSubtree(TreeNode* root) {
+        if(root == NULL)
+            return 0;
+        dfs(root);
+        return maxCount;
+    }
+};
