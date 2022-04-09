70a385debc779620af9c579d67658e7e8d43a518
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-04-09 10:13:14 -0700

    Time: 0 ms (100.00%), Space: 8.4 MB (43.18%) - LeetHub

---
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    enum Zones{
+        ARRIVAL,
+        INTERIM,
+        DEPARTURE        
+    };
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        if(root == NULL)
+            return result;
+        stack<pair<TreeNode*, Zones>> s;
+        s.push({root, ARRIVAL});
+        while(!s.empty())
+        {
+            TreeNode* node = s.top().first;
+            Zones currZone = s.top().second;
+            if(currZone == ARRIVAL)
+            {
+                currZone = INTERIM;
+                s.pop();
+                s.push({node, currZone});
+                if(node->left)
+                    s.push({node->left, ARRIVAL});
+            }
+            else if(currZone == INTERIM)
+            {
+                currZone = DEPARTURE;
+                s.pop();
+                s.push({node, currZone});
+                result.push_back(node->val);
+                if(node->right)
+                    s.push({node->right, ARRIVAL});
+            }
+            else if(currZone == DEPARTURE)
+            {
+                s.pop();
+            }
+        }
+        return result;
+    }
+};
