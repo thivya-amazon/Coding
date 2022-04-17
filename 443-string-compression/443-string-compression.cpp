0797adaad265149891d04294b441d1e9d99f2c64
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-04-17 00:47:39 -0700

    Time: 6 ms (63.05%), Space: 9 MB (68.21%) - LeetHub

---
+++ 443-string-compression/443-string-compression.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        if(chars.size() <= 1)
+            return chars.size();
+        int count = 0;
+        int charIdx = -1;
+        for(int i = 0; i < chars.size(); )
+        {
+            int index = i + 1;
+            int charCount = 1;
+            while(index < chars.size() && chars[i] == chars[index])
+            {
+                index++;
+                charCount++;
+            }
+            chars[++charIdx] = chars[i];
+            if(charCount > 1)
+            {
+                if(charCount >= 10)
+                {
+                    vector<char> temp;
+                    while(charCount > 0)
+                    {
+                        temp.push_back(charCount % 10 + '0');
+                        charCount /= 10;
+                    }
+                    reverse(temp.begin(), temp.end());
+                    for(int j = 0; j < temp.size(); j++)
+                        chars[++charIdx] = temp[j];
+                    count = count + 1 + temp.size();
+                }
+                else
+                {
+                    chars[++charIdx] = charCount + '0';
+                    count += 2;
+                }
+
+            }
+            else
+                count++;           
+            i = index;
+            
+        }
+        return count;
+    }
+};
