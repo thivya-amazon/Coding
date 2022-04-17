ee56a8534ebe647389106c753bea5784c9298deb
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-04-17 14:31:20 -0700

    Time: 4 ms (65.04%), Space: 7.1 MB (53.85%) - LeetHub

---
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int result = 0;
+        long long tempResult = 0;
+        int sign = 1;
+        
+        if(s.size() < 1)
+            return 0;
+        //Remove leading whitespaces
+        int i = 0;
+        while(i < s.size())
+        {
+            if(isspace(s[i]))
+                s.erase(s.begin());
+            else
+                break;
+        }
+        //Collect the sign
+            if(s[0] == '+')
+            {
+                sign = 1;
+                s.erase(s.begin());
+            }
+            else if(s[0] == '-')
+            {
+                sign = -1;
+                s.erase(s.begin());
+            }               
+
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(isdigit(s[i]))
+            {
+                int digit = s[i] - '0';
+                if((tempResult > INT_MAX / 10) || (tempResult == INT_MAX/10 && digit > INT_MAX % 10 ))
+                {
+                    if(sign == 1)
+                        return INT_MAX;
+                    else
+                        return INT_MIN;
+                }
+
+                tempResult = tempResult*10 + digit;
+            }
+            else
+                break;
+        }
+        if((sign * tempResult) <= INT_MIN)
+            result = INT_MIN;
+        else if(tempResult > INT_MAX)
+            result = INT_MAX;
+        else
+            result = sign * tempResult;
+        
+        return result;
+    }
+};
