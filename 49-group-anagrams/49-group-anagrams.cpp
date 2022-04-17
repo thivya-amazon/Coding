5f52c9712b6411a944bc84ff8f2a951eae4fe8c0
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-04-17 13:13:18 -0700

    Time: 131 ms (9.41%), Space: 24.5 MB (14.24%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> result;
+        vector<int> count(26, 0);
+        unordered_map<string, vector<string>> umap;
+        for(auto s : strs)
+        {
+            for(int i = 0; i < s.size(); i++)
+                count[s[i] - 'a']++;
+            string key = "";
+            for(int j = 0; j < 26; j++)
+            {
+                key.push_back('#');
+                key.append(to_string(count[j]));
+            }
+            umap[key].push_back(s);
+            //Reset the count vector
+            for(int j = 0; j < 26; j++)
+            {
+                count[j] = 0;
+            }
+        }
+        for(auto i = umap.begin(); i != umap.end(); i++)
+        {
+            result.push_back(i->second);
+        }
+        return result;
+    }
+};
