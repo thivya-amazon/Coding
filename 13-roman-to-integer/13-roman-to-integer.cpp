dd003c01ad9a2c91d72fe7c0f1c20d37d6111ccb
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-04-17 00:17:24 -0700

    Time: 11 ms (72.28%), Space: 6 MB (87.37%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int value(char c)
+    {
+        switch(c)
+        {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
+    }
+    int romanToInt(string s) {
+        int first = 0;
+        int second = 0;
+        int result = 0;
+        for(int i = 0; i < s.size(); i++)
+        {
+            first = value(s[i]);
+            if(i+1 < s.size())
+            {
+                second = value(s[i+1]);
+                if(first < second)
+                {
+                    result = result + second - first;
+                    i++;
+                }                    
+                else
+                    result = result + first;
+            }
+            else
+                result = result + first;
+        }
+        return result;
+    }
+};
