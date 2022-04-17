041a4bbb4c44af788354fb8b6f906ded855f315a
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-04-16 23:40:45 -0700

    Time: 15 ms (26.60%), Space: 7.1 MB (80.43%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -13,37 +13,36 @@ public:
         reverse(s.begin(), s.end());
         
         int start_idx = 0;
+        //Erase leading whitespaces
         while(isspace(s[start_idx]) && s[start_idx] != NULL)
         {
             s.erase(s.begin() + start_idx);
         }
         
         int end_idx = s.size()-1;
+        //Erase trailing whitespaces
         while(isspace(s[end_idx]) && s[end_idx] != NULL)
         {
             s.erase(s.begin() + end_idx);
             end_idx--;
         }
-        start_idx = 0;
-        end_idx = 0;
-        while(end_idx < s.size())
+        int index = 0;
+        while(index < s.size())
         {
-            int wordStart = start_idx;
+            int wordStart = index;
             while(isspace(s[wordStart]))
             {
                 s.erase(s.begin()+wordStart);
-                //wordStart++; 
             }
                 
-            while(!isspace(s[start_idx]) && s[start_idx] != NULL)
+            while(!isspace(s[index]) && s[index] != NULL)
             {
-               end_idx++;
-                start_idx++;
+                index++;
             }
                 
-            revWord(s, wordStart, end_idx-1);
-            start_idx = end_idx+1;
-            end_idx = start_idx;
+            revWord(s, wordStart, index-1);
+            //skip the white space and make the index point the start of the next word
+            index++;
         }
         return s;
     }
