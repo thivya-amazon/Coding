41b842f52f33adbb4723ec89d6dd36c908275e07
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-04-17 00:09:03 -0700

    Time: 448 ms (5.00%), Space: 7.4 MB (51.44%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -8,14 +8,19 @@ public:
         {
             if((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z') || (s[i] >= '0' && s[i] <= '9'))
             {
-                letters.push_back(tolower(s[i]));
-            }                
+                s[i] = tolower(s[i]);
+            }
+            else
+            {
+                s.erase(s.begin() + i);
+                i--;
+            }
         }
        int start = 0;
-        int end = letters.size()-1;
+        int end = s.size()-1;
         while(start < end)
         {
-            if(letters[start++] != letters[end--])
+            if(s[start++] != s[end--])
                 return false;
         }
         return true;
