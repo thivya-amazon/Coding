42ed14dfcf93d452bfc4079e08e27f504b31f564
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-04-17 17:27:55 -0700

    Time: 0 ms (100.00%), Space: 6.4 MB (23.09%) - LeetHub

---
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        if(s.size() == 0)
+            return 0;
+        stack<char> stk;
+        int count = 0;
+        
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(s[i] == '(')
+                stk.push(s[i]);
+            else if(s[i] == ')')
+            {
+                if(stk.empty())
+                    s[i] = '$';
+                else
+                    stk.pop();
+            }
+        }
+        while(!stk.empty())
+        {
+            count++;
+            stk.pop();
+        }
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(s[i] == '$')
+                count++;
+        }
+        return count;
+    }
+};
