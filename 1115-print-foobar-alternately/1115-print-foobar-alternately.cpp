b8f8dac0a0ed6fba703456253310b3d63d7ed8e1
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-04-23 17:20:22 -0700

    Time: 868 ms (5.96%), Space: 8.4 MB (31.10%) - LeetHub

--- 1115-print-foobar-alternately/1115-print-foobar-alternately.cpp
+++ 1115-print-foobar-alternately/1115-print-foobar-alternately.cpp
@@ -9,28 +9,28 @@ private:
 public:
     FooBar(int n) {
         this->n = n;
-        sem_init(&fooSem, 0, 0);
+        sem_init(&fooSem, 0, 1);
         sem_init(&barSem, 0, 0);
     }
 
     void foo(function<void()> printFoo) {
         
         for (int i = 0; i < n; i++) {
-            if(i != 0)
-                sem_wait(&barSem);
+ 
+            sem_wait(&fooSem);
         	// printFoo() outputs "foo". Do not change or remove this line.
         	printFoo();
-            sem_post(&fooSem);
+            sem_post(&barSem);
         }
     }
 
     void bar(function<void()> printBar) {
         
         for (int i = 0; i < n; i++) {
-            sem_wait(&fooSem);
+            sem_wait(&barSem);
         	// printBar() outputs "bar". Do not change or remove this line.
         	printBar();
-            sem_post(&barSem);
+            sem_post(&fooSem);
         }
     }
 };
