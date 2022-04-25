98413171d0874e62dbaaf2f9ea1533fd3d65991a
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-04-25 11:47:03 -0700

    Time: 2 ms (35.18%), Space: 5.9 MB (23.39%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -9,15 +9,15 @@ public:
         while(start <= end)
         {
             int mid = start + (end-start)/2;
-            if(isBadVersion(mid) == true && isBadVersion(mid-1) == false)
-                return mid;
-            else if(isBadVersion(mid) == false && isBadVersion(mid+1) == true)
-                return mid+1;
-            else if(isBadVersion(mid) == true)
+            // if(isBadVersion(mid) == true && isBadVersion(mid-1) == false)
+            //     return mid;
+            // else if(isBadVersion(mid) == false && isBadVersion(mid+1) == true)
+            //     return mid+1;
+            if(isBadVersion(mid) == true)
                 end = mid-1;
             else
                 start = mid+1;
         }
-        return -1;
+        return start;
     }
 };
