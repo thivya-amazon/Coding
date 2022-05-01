5ed9a61d6340b47fa259fe300317516a82943db0
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-04-30 20:53:15 -0700

    Time: 113 ms (42.25%), Space: 36 MB (97.41%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -7,16 +7,17 @@ public:
         for(int i = 0; i < nums.size(); i++)
         {
             prefixSum += nums[i];
+            
             if(prefixSum == k)
                 globalCount += 1;
             
             if(umap.find(prefixSum - k) != umap.end())
                 globalCount += umap[prefixSum - k];
             
-           // if(umap.find(prefixSum) != umap.end())
+            if(umap.find(prefixSum) != umap.end())
                 umap[prefixSum] += 1;
-            // else
-            //     umap[prefixSum] = 1;            
+            else
+                umap[prefixSum] = 1;            
         }
         return globalCount;
     }
