3f95d9d4aacade6b04c1712747bf50e7195b038b
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-04-30 22:08:32 -0700

    Time: 185 ms (68.25%), Space: 52 MB (88.75%) - LeetHub

---
+++ 325-maximum-size-subarray-sum-equals-k/325-maximum-size-subarray-sum-equals-k.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxSubArrayLen(vector<int>& nums, int k) {
+        int maxLength = 0;
+        int prefixSum = 0;
+        //Stores prefix sum of subarray as key and its length as value
+        unordered_map<int,int> umap;
+        
+        for(int i = 0; i < nums.size(); i++)
+        {
+            prefixSum += nums[i];
+            if(prefixSum == k)
+                maxLength = i+1;
+            
+            if(umap.find(prefixSum - k) != umap.end())
+                maxLength = max(maxLength, (i+1) - umap[prefixSum - k]);
+            
+            if(umap.find(prefixSum) == umap.end())
+                umap[prefixSum] = i+1;
+        }
+        return maxLength;
+    }
+};
