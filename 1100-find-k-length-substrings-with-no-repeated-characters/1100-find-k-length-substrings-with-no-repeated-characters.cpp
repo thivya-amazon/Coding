30c3a5506c530acf40ee5c17484d9fbc3d858f80
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-05-04 13:37:04 -0700

    Time: 12 ms (44.06%), Space: 8 MB (29.37%) - LeetHub

---
+++ 1100-find-k-length-substrings-with-no-repeated-characters/1100-find-k-length-substrings-with-no-repeated-characters.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int numKLenSubstrNoRepeats(string s, int k) {
+        int count = 0;
+        
+        if(k > s.size())
+            return 0;
+        unordered_map<char, int> chars(k);
+        for(int i = 0; i < k; i++)
+        {
+            if(chars.count(s[i]))
+            {
+                chars[s[i]]++;
+            }             
+            else
+                chars[s[i]] = 1;
+        }
+        if(chars.size() == k)
+            count++;
+        for(int i = k; i < s.size(); i++)
+        {            
+            //Remove the leftmost element in the window
+            chars[s[i-k]]--;
+            if(chars[s[i-k]] == 0)
+                chars.erase(s[i-k]);
+
+            if(chars.count(s[i]))
+            {
+                chars[s[i]]++;
+            }             
+            else
+                chars[s[i]] = 1;
+            
+            if(chars.size() == k)
+                count++;
+
+        }
+        return count;
+    }
+};
