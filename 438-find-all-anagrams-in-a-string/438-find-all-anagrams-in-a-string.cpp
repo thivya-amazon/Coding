3643c1bc290114121699e4a0e01782ca7e82d621
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-05-04 19:30:37 -0700

    Time: 17 ms (65.23%), Space: 8.7 MB (73.11%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -1,37 +1,26 @@
 class Solution {
 public:
-
     vector<int> findAnagrams(string s, string p) {
         vector<int> result;
-        vector<int> shash(26, 0);
-        vector<int> phash(26, 0);
-        int window = p.size();
-        int left = 0;
-        int right = 0;
-        
         if(p.size() > s.size())
             return result;
-        while(right < window)
+        vector<int> phash(26, 0);
+        vector<int> shash(26, 0);
+        
+        int k  = p.size();
+        for(int i = 0; i < k; i++)
         {
-            phash[p[right] - 'a']++;
-            shash[s[right] - 'a']++;
-            right++;
+            phash[p[i] - 'a']++;
+            shash[s[i] - 'a']++;
         }
-        //Reset right back to the next element past the window
-        right--;
-        while(right < s.size())
+        if(phash == shash)
+            result.push_back(0);
+        for(int i = k; i < s.size(); i++)
         {
+            shash[s[i] - 'a']++;
+            shash[s[i-k] - 'a']--;
             if(phash == shash)
-                result.push_back(left);
-            
-            //Slide the window
-            right++;
-            if(right != s.size())
-            {
-                shash[s[right] - 'a']++;
-                shash[s[left] - 'a']--;
-            }
-            left++;
+                result.push_back(i-k+1);
         }
         return result;
     }
