40a1b386dee4cb396c8f3bc3399cd9392b7bdd5b
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-05-05 23:14:23 -0700

    Time: 270 ms (31.73%), Space: 96.5 MB (56.19%) - LeetHub

--- 523-continuous-subarray-sum/523-continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.cpp
@@ -7,15 +7,15 @@ public:
         unordered_map<int, int> umap;
         
         //Special case for k = 0
-        if(k == 0)
-        {
-            for(int i = 0; i < nums.size() - 1; i++)
-            {
-                if(nums[i] == 0 && nums[i+1] == 0)
-                    return true;
-            }
-            return false;
-        }
+        // if(k == 0)
+        // {
+        //     for(int i = 0; i < nums.size() - 1; i++)
+        //     {
+        //         if(nums[i] == 0 && nums[i+1] == 0)
+        //             return true;
+        //     }
+        //     return false;
+        // }
 
         for(int i = 0; i < nums.size(); i++)
         {
