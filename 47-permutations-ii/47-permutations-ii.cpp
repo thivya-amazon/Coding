502eb9cfd73b76194a199ba01ceed6deb5f0b487
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-05-06 16:46:40 -0700

    Time: 13 ms (52.59%), Space: 9.6 MB (44.46%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+vector<vector<int>> result;
+    void permHelper(int sub_idx, vector<int> nums)
+    {
+        if(sub_idx == nums.size())
+        {
+            result.push_back(nums);
+            return;
+        }
+        for(int i = sub_idx; i < nums.size(); i++)
+        {
+            if(i == sub_idx || (nums[i] != nums[i-1] && nums[i] != nums[sub_idx]))
+            {
+                swap(nums[i], nums[sub_idx]);
+                permHelper(sub_idx+1, nums);
+                //swap(nums[i], nums[sub_idx]);
+            }
+        }
+    }
+
+vector<vector<int>> permuteUnique(vector<int>& nums) {        
+        sort(nums.begin() , nums.end());
+        permHelper(0, nums);
+        return result;
+    }
+};
