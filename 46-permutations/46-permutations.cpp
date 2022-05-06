3beadb280d0bc39d273fbce8aba912f68284d70d
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-05-06 16:31:18 -0700

    Time: 0 ms (100.00%), Space: 8 MB (38.30%) - LeetHub

---
+++ 46-permutations/46-permutations.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> result;
+    void permHelper(int sub_idx, vector<int>& nums)
+    {
+        if(sub_idx == nums.size())
+        {
+            result.push_back(nums);
+            return;
+        }
+        for(int i = sub_idx; i < nums.size(); i++)
+        {
+            swap(nums[i], nums[sub_idx]);
+            permHelper(sub_idx+1, nums);
+            swap(nums[i], nums[sub_idx]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        permHelper(0, nums);
+        return result;
+    }
+};
