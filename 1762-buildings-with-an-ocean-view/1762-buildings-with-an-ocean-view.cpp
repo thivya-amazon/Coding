05c06c6cb2bfc659955d620a5abddad7a741f75e
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-05-08 16:41:47 -0700

    Time: 194 ms (40.87%), Space: 69.5 MB (12.82%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.cpp
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     vector<int> findBuildings(vector<int>& heights) {
-        //stack to push the index of each building
-        stack <int> s;
+        stack<int> s;
         vector<int> result;
         for(int i = 0; i < heights.size(); i++)
         {
-            // if(!s.empty())
-            // {
-            //     if(heights[i] >= heights[s.top()])
-                    while(!s.empty() && heights[i] >= heights[s.top()])
-                        s.pop();                
-            //}
+            while(!s.empty() && heights[i] >= heights[s.top()])
+                s.pop();
             s.push(i);
-        }        
+        }
         while(!s.empty())
         {
             result.push_back(s.top());
             s.pop();
         }
         reverse(result.begin(), result.end());
-        return result;
+        return result;    
+        
     }
 };
