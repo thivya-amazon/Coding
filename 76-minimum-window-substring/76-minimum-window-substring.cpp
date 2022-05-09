61d7cb23f6bc44958ad7d6af1ef8685bc6f401ce
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-05-09 11:19:39 -0700

    Time: 33 ms (39.99%), Space: 7.8 MB (68.24%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        unordered_map<char, int> umap;
-        
-        //Insert all characters in t into a map
-        for(int i = 0; i < t.size(); i++)
-            umap[t[i]]++;
-        
-        int left= 0;
-        int right = 0;
-        int startIdx = 0;
+        unordered_map<char, int> tmap;
+        int left = 0;
+        int count = 0;
         int minLen = INT_MAX;
-        int count = umap.size();
-        while(right < s.size())
+        int startIdx = -1;
+        
+        for(int i = 0; i < t.size(); i++)
+            tmap[t[i]]++;
+        
+        count = tmap.size();
+        for(int right = 0; right < s.size(); right++)
         {
-            umap[s[right]]--;
-            if(umap[s[right]] == 0)
+            tmap[s[right]]--;
+            if(tmap[s[right]] == 0)
                 count--;
             if(count == 0)
             {
@@ -23,22 +22,20 @@ public:
                 {
                     if(right-left+1 < minLen)
                     {
-                        minLen = right-left+1;
+                        minLen = right - left + 1;
                         startIdx = left;
                     }
-                
-                    umap[s[left]]++;
-                    if(umap[s[left]] > 0)
-                        count++;
+                    
+                    //Shrink the window from the left
+                    tmap[s[left]]++;
+                    if(tmap[s[left]] > 0)
+                        count++;                   
                     left++;
                 }
-            }
-            right++;
+            }            
         }
-        
-        if(minLen == INT_MAX)
+        if (minLen == INT_MAX)
             return "";
         return s.substr(startIdx, minLen);
-
     }
 };
