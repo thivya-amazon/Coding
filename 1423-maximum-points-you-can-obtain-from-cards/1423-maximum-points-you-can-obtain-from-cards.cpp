f0c792238308ad5c97cc22fe928d574087efb8c9
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-05-08 21:06:51 -0700

    Time: 141 ms (5.07%), Space: 42.5 MB (51.97%) - LeetHub

---
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    //Minimize the score for (n-k) window. So, that the remaining elements' score is maximized
+    int maxScore(vector<int>& cardPoints, int k) {
+        int windowSize = cardPoints.size() - k;
+        int windowSum = 0;
+        int minScore = INT_MAX;
+        for(int i = 0; i < windowSize; i++)
+        {
+            windowSum += cardPoints[i];
+        }
+        minScore = min(minScore, windowSum);
+        for(int i = windowSize; i < cardPoints.size(); i++)
+        {
+            windowSum += cardPoints[i] - cardPoints[i-windowSize];
+            if(windowSum < minScore)
+            {
+                minScore = windowSum;
+            }
+        }
+        
+        int totalScore = 0;
+        for(auto i : cardPoints)
+            totalScore += i;
+        
+        return totalScore - minScore;
+        
+    }
+};
