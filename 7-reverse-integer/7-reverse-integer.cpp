0ac3550c0d07c3f5ea5e9c05192fc01bd92b2ade
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-05-11 21:10:28 -0700

    Time: 0 ms (100.00%), Space: 6 MB (34.42%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.cpp
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -8,9 +8,9 @@ public:
         int result = 0;
         while(x)
         {
-            if(result > INT_MAX/10)
+            if(result > INT_MAX/10 || (result == INT_MAX/10 && (x % 10 > 7)))
                 return 0;
-            if(result < INT_MIN/10)
+            if(result < INT_MIN/10 || (result == INT_MIN/10 && (x % 10 < -8)))
                 return 0;
             result = (result*10) + (x % 10);
             x = x / 10;
