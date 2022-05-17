c483813a87652daccf2e9676009cfa86ad19fdc5
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-05-16 18:58:23 -0700

    Time: 0 ms (100.00%), Space: 10.1 MB (72.51%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    //Zones : |
     int findMin(vector<int>& nums) {
         int n = nums.size();
-        //Check if the array is not rotated
+        
+        //Array not rotated
         if(nums[0] < nums[n-1])
             return nums[0];
         int start = 0;
@@ -11,7 +11,6 @@ public:
         while(start <= end)
         {
             int mid = start + (end-start)/2;
-            //Compare to the polestar value to determine the zone
             if(nums[mid] > nums[n-1])
                 start = mid + 1;
             else
