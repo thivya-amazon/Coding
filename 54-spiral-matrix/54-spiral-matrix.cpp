332fb01404c11aa014cded2725414b632a87e24b
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-05-19 15:24:03 -0700

    Time: 4 ms (25.66%), Space: 6.9 MB (31.76%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -8,31 +8,45 @@ public:
         int bottom = ROWS - 1;
         int left = 0;
         int right = COLS - 1;
+        int direction = 1;
         while(result.size() < ROWS * COLS)
         {
             //1. Traverse left to right & move the top boundary after the row is traversed
-            for(int i = left; i <= right; i++)
-                result.push_back(matrix[top][i]);
-            top++;
-            
-            //2. Traverse from top to bottom and move the right boundary after the column is traversed
-            for(int i = top; i <= bottom; i++)
-                result.push_back(matrix[i][right]);
-            right--;
-            
-            if(left > right || top > bottom)
-                break; 
+            if(direction == 1)
+            {
+                for(int i = left; i <= right; i++)
+                    result.push_back(matrix[top][i]);
+                top++;
+                direction = 2;
+            }
+
+             //2. Traverse from top to bottom and move the right boundary after the column is traversed
+            else if(direction == 2)
+            {
+                for(int i = top; i <= bottom; i++)
+                    result.push_back(matrix[i][right]);
+                right--;
+                direction = 3;
+            }
             
             //3. Traverse from right to left and move the bottom boundary after the row is traversed
-            for(int i = right; i >= left; i--)
-                result.push_back(matrix[bottom][i]);
-            bottom--;
-        
+            else if(direction == 3)
+            {
+                for(int i = right; i >= left; i--)
+                    result.push_back(matrix[bottom][i]);
+                bottom--;
+                direction = 4;
+            }  
             
             //4. Traverse from bottom to top and move the left boundary after the column is traversed
-            for(int i = bottom; i >= top; i--)
-                result.push_back(matrix[i][left]);
-            left++;           
+            else if(direction == 4)
+            {
+                for(int i = bottom; i >= top; i--)
+                    result.push_back(matrix[i][left]);
+                left++;
+                direction = 1;
+            }
+                     
         }
         return result;
     }
