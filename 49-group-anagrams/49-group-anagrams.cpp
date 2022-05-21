66e638658943e7fb4b48a6aeb29edd0e42516f26
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-05-21 16:06:01 -0700

    Time: 42 ms (71.25%), Space: 24.3 MB (14.88%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -2,24 +2,30 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> result;
-        vector<int> count(26, 0);
         unordered_map<string, vector<string>> umap;
-        for(auto s : strs)
+        vector<int> charCount(26, 0);
+        for(auto s  : strs)
         {
+            //1. Count the chars in every string
             for(int i = 0; i < s.size(); i++)
-                count[s[i] - 'a']++;
+                charCount[s[i] - 'a']++;
+            
+            //2. Form a key of count of chars with delimiters 
             string key = "";
             for(int j = 0; j < 26; j++)
             {
-                //Without having a delimiter multi-digit count values cannot be differentiated
                 key.push_back('#');
-                key.append(to_string(count[j]));
+                key.push_back(charCount[j]);
             }
+            
+            //3. Insert key into umap with value as a vector of strings corresponding to the key generated ins tep 2
             umap[key].push_back(s);
-            //Reset the count vector
+            
+            //4.Reset count vector for the next string
             for(int i = 0; i < s.size(); i++)
-                count[s[i] - 'a']--;
+                charCount[s[i] - 'a']--;
         }
+        
         for(auto i = umap.begin(); i != umap.end(); i++)
         {
             result.push_back(i->second);
