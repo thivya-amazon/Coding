5412f03a553cfab056cf6d9eb24f70841bf6e5ef
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-05-21 15:32:06 -0700

    Time: 59 ms (12.24%), Space: 10 MB (61.21%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,13 +1,40 @@
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int, vector<int>, greater<int>> minHeap;
-        for(int i = 0; i < nums.size(); i++)
+    int partition(vector<int>& nums, int start, int end)
+    {
+        // int randIdx = start + rand() % (end - start);
+        // swap(nums[randIdx], nums[start]);
+        
+        int pivot = nums[start];
+        int smaller = start;
+        for(int bigger = start+1; bigger <= end; bigger++)
         {
-            minHeap.push(nums[i]);
-            if(minHeap.size() > k)
-                minHeap.pop();
+            if(nums[bigger] < pivot)
+            {
+                smaller++;
+                swap(nums[bigger], nums[smaller]);
+            }
         }
-        return minHeap.top();
+        swap(nums[start], nums[smaller]);
+        return smaller;
+    }
+    int quickSelect(vector<int>& nums, int start, int end, int k)
+    {
+        int pivotIdx = 0;
+        while(1)
+        {            
+            pivotIdx = partition(nums, start, end);
+            if(pivotIdx == k)
+                break;
+            if(pivotIdx < k)
+                start = pivotIdx + 1;
+            else
+                end = pivotIdx - 1;
+        }
+        return nums[pivotIdx];
+    }
+    int findKthLargest(vector<int>& nums, int k) {
+        int n = nums.size();
+        return quickSelect(nums, 0, n-1, n-k);
     }
 };
