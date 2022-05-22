5dae87b837897c736a3de4e201da4dc31653b219
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-05-21 17:16:43 -0700

    Time: 147 ms (96.58%), Space: 83 MB (42.13%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         int n = nums.size();
-        for(int i = 0; i < n; i++)
+        for(int i = 0; i < nums.size(); i++)
         {
             while(nums[i] != i+1)
             {
-               unsigned long destIndex = (unsigned long)nums[i] - 1;
-                if(destIndex >= 0 && destIndex < n && nums[i] != nums[destIndex])
-                    swap(nums[i], nums[destIndex]);
+                unsigned int destIdx = (unsigned int)nums[i] - 1;
+                if(destIdx >= 0 && destIdx < n && nums[i] != nums[destIdx])
+                    swap(nums[i], nums[destIdx]);
                 else
                     break;
             }
         }
-        
         for(int i = 0; i < n; i++)
         {
             if(nums[i] != i+1)
