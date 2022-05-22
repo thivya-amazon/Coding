a9ff77bcff6394c2c4dbec306c3d2b15cde01760
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-05-22 16:00:27 -0700

    Time: 16 ms (75.01%), Space: 7.6 MB (83.17%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -6,11 +6,10 @@ public:
         int count = 0;
         int minLen = INT_MAX;
         int startIdx = -1;
-        
         for(int i = 0; i < t.size(); i++)
             tmap[t[i]]++;
-        
         count = tmap.size();
+        
         for(int right = 0; right < s.size(); right++)
         {
             tmap[s[right]]--;
@@ -20,21 +19,20 @@ public:
             {
                 while(count == 0)
                 {
-                    if(right-left+1 < minLen)
+                    if(right - left + 1 < minLen)
                     {
-                        minLen = right - left + 1;
+                        minLen = right-left+1;
                         startIdx = left;
                     }
-                    
-                    //Shrink the window from the left
                     tmap[s[left]]++;
                     if(tmap[s[left]] > 0)
-                        count++;                   
+                        count++;
                     left++;
+    
                 }
-            }            
+            }
         }
-        if (minLen == INT_MAX)
+        if(minLen == INT_MAX)
             return "";
         return s.substr(startIdx, minLen);
     }
