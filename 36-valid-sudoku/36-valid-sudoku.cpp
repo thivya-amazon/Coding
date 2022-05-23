48b30bcb37f26f532e2d268d7d70e45bac6771ee
thivyaashok14 <99231372+thivyaashok14@users.noreply.github.com>
2022-05-22 19:29:22 -0700

    Time: 56 ms (8.58%), Space: 19.9 MB (39.02%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-    unordered_set<string> uset;
+        unordered_set<string> uset;
         for(int i = 0; i < 9; i++)
             for(int j = 0; j < 9; j++)
             {
@@ -9,16 +9,18 @@ public:
                 {
                     string row = "r-" + to_string(i) + to_string(board[i][j]);
                     string col = "c-" + to_string(j) + to_string(board[i][j]);
-                    int x = i/3;
-                    int y = j/3;
+                    int x = i / 3;
+                    int y = j / 3;
                     string box = "b-" + to_string(x) + "-" + to_string(y) + to_string(board[i][j]);
+
                     if(uset.count(row) || uset.count(col) || uset.count(box))
                         return false;
                     uset.insert(row);
                     uset.insert(col);
-                    uset.insert(box);
+                    uset.insert(box);    
                 }
             }
+        
         return true;
     }
 };
